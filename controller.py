3f503fc3d29d2fef0ba057916b869382fa388661
vivekagra <vivekagr@iitk.ac.in>
2020-09-29 11:10:15 +0530

    Update controller.py

--- controller.py
+++ controller.py
@@ -18,74 +18,74 @@ class Controller:
     
     def run(self):
         if(self.flag == 1):
-            self.kp(0,0)=25     #hover
-            self.kp(1,1)=1
-            self.kp(2,2)=40
-            self.kv(0,0)=15
-            self.kv(1,1)=6
-            self.kv(2,2)=0
-            self.kr(0,0)=3
-            self.kr(1,1)=45
-            self.kr(2,2)=5
-            self.kw(0,0)=5
-            self.kw(1,1)=70
-            self.kw(2,2)=1
+            self.kp[0,0]=25     #hover
+            self.kp[1,1]=1
+            self.kp[2,2]=40
+            self.kv[0,0]=15
+            self.kv[1,1]=6
+            self.kv[2,2]=0
+            self.kr[0,0]=3
+            self.kr[1,1]=45
+            self.kr(2,2]=5
+            self.kw[0,0]=5
+            self.kw[1,1]=70
+            self.kw[2,2]=1
             
         elif (self.flag==2):
-            self.kp(0,0)=0     #10      forward tran, kpx nt impacting 
-            self.kp(1,1)=0
-            self.kp(2,2)=300     #50
-            self.kv(0,0)=180     #15
-            self.kv(1,1)=100
-            self.kv(2,2)=120         #40
-            self.kr(0,0)=0
-            self.kr(1,1)=200
-            self.kr(2,2)=0
-            self.kw(0,0)=1
-            self.kw(1,1)=200     #if after some seconds the code is not working then change kw and kr. Make inner looprun faster
-            self.kw(2,2)=1
+            self.kp[0,0]=0     #10      forward tran, kpx nt impacting 
+            self.kp[1,1]=0
+            self.kp[2,2]=300     #50
+            self.kv[0,0]=180     #15
+            self.kv[1,1]=100
+            self.kv[2,2]=120         #40
+            self.kr[0,0]=0
+            self.kr[1,1]=200
+            self.kr[2,2]=0
+            self.kw[0,0]=1
+            self.kw[1,1]=200     #if after some seconds the code is not working then change kw and kr. Make inner looprun faster
+            self.kw[2,2]=1
     
         elif (self.flag==3):
-            self.kp(0,0)=0
-            self.kp(1,1)=1      #cruise
-            self.kp(2,2)=20
-            self.kv(0,0)=70 #30
-            self.kv(1,1)=1
-            self.kv(2,2)=30
-            self.kr(0,0)=1
-            self.kr(1,1)=250 #50
-            self.kr(2,2)=1
-            self.kw(0,0)=1
-            self.kw(1,1)=500 #70
-            self.kw(2,2)=1   
+            self.kp[0,0]=0
+            self.kp[1,1]=1      #cruise
+            self.kp[2,2]=20
+            self.kv[0,0]=70 #30
+            self.kv[1,1]=1
+            self.kv[2,2]=30
+            self.kr[0,0]=1
+            self.kr[1,1]=250 #50
+            self.kr[2,2]=1
+            self.kw[0,0]=1
+            self.kw[1,1]=500 #70
+            self.kw[2,2]=1   
     
         elif (self.flag==4):
-            self.kp(0,0)=5
-            self.kp(1,1)=1    #backtrans
-            self.kp(2,2)=1
-            self.kv(0,0)=5
-            self.kv(1,1)=1
-            self.kv(2,2)=30
-            self.kr(0,0)=1
-            self.kr(1,1)=70
-            self.kr(2,2)=1
-            self.kw(0,0)=1
-            self.kw(1,1)=200
-            self.kw(2,2)=1
+            self.kp[0,0]=5
+            self.kp[1,1]=1    #backtrans
+            self.kp[2,2]=1
+            self.kv[0,0]=5
+            self.kv[1,1]=1
+            self.kv[2,2]=30
+            self.kr[0,0]=1
+            self.kr[1,1]=70
+            self.kr[2,2]=1
+            self.kw[0,0]=1
+            self.kw[1,1]=200
+            self.kw[2,2]=1
     
         elif (self.flag==5):
-            self.kp(0,0)=5
-            self.kp(1,1)=1     #land
-            self.kp(2,2)=1
-            self.kv(0,0)=5
-            self.kv(1,1)=1
-            self.kv(2,2)=30
-            self.kr(0,0)=1
-            self.kr(1,1)=70
-            self.kr(2,2)=1
-            self.kw(0,0)=1
-            self.kw(1,1)=1030
-            self.kw(2,2)=1
+            self.kp[0,0]=5
+            self.kp[1,1]=1     #land
+            self.kp[2,2]=1
+            self.kv[0,0]=5
+            self.kv[1,1]=1
+            self.kv[2,2]=30
+            self.kr[0,0]=1
+            self.kr[1,1]=70
+            self.kr[2,2]=1
+            self.kw[0,0]=1
+            self.kw[1,1]=1030
+            self.kw[2,2]=1
         
         ud = self.des_state.control
         omega_curr=[[1, 0, -sin(self.state.rot(2))], [0, cos(self.state.rot(3)), cos(state.rot(2))*sin(state.rot(3))], [0, -sin(state.rot(3)), cos(state.rot(2))*cos(state.rot(3))]] * [[state.omega(1)], [state.omega(2)], [state.omega(3)]]
